8c1986fd17997481d61930235239983b07c7ac37
kartik3110 <kartik31.gupta@gmail.com>
2024-04-09 22:38:07 +0530

    minor changes with no effect

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,6 @@ import Navbar from "../components/Home/Navbar";
 import CallToAction from "../components/Home/CallToAction";
 import Section2 from "../components/Home/Section2";
 import Services from "../components/Home/Services";
-import FrameComponent3 from "../components/Home/FrameComponent3";
-import FrameComponent2 from "../components/Home/FrameComponent2";
 import Benefits from "../components/Home/Benefits";
 import Testimonials from "../components/Home/Testimonials";
 import Blog from "../components/Home/Blog";
@@ -21,8 +19,6 @@ const Home = () => {
       <CallToAction />
       <Section2 />
       <Services />
-      {/* <FrameComponent3 /> */}
-      {/* <FrameComponent2 /> */}
       <Benefits />
       <Testimonials />
       <Blog />
